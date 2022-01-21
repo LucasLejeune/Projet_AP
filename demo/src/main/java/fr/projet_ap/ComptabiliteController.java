ce1c86c2c55ae29150c520b6eeae10e9571ebd75
LucasLejeune <lucaslejeune0@gmail.com>
2022-01-21 11:35:06 +0100

    Mise en place d'une connexion admin

--- demo/src/main/java/fr/projet_ap/ComptabiliteController.java
+++ demo/src/main/java/fr/projet_ap/ComptabiliteController.java
@@ -29,37 +29,63 @@ public class ComptabiliteController {
         String username = "root";
         String password = "";
         Boolean bonId = false;
-        
+
         try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
 
-            String sql = "SELECT identifiant, Mot_de_passe, role FROM users";
- 
+            String sql = "SELECT Matricule, Mot_de_passe, ta_role FROM users join type_agent ON ta_fk = ta_id; ";
+
             Statement statement = conn.createStatement();
             ResultSet result = statement.executeQuery(sql);
 
-            
-            while (result.next()){
+            while (result.next()) {
                 String ident = result.getString(1);
                 String pass = result.getString(2);
                 String role = result.getString(3);
-                
-                if (id.getText().equals(ident) && mdp.getText().equals(pass) && role.equals("Compta")){
+
+                if (id.getText().equals(ident) && mdp.getText().equals(pass) && role.equals("compta")
+                        || id.getText().equals("admin") && mdp.getText().equals("admin")) {
                     bonId = true;
                     App.setRoot("ClientC");
-                }               
+                }
             }
-            if (bonId == false){
+            if (bonId == false) {
                 pasBon.setText("Identifiant ou mot de passe invalide");
             }
-             
+
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        
+
     }
 
     @FXML
     private void switchToAcceuil() throws IOException {
         App.setRoot("Acceuil");
     }
+
+    private void validation() throws IOException {
+        String dbURL = "jdbc:mysql://localhost:3306/projet_AP";
+        String username = "root";
+        String password = "";
+
+        try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
+
+            String sql = "SELECT Matricule, nom, prenom FROM users;";
+
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+
+            while (result.next()) {
+                String ident = result.getString(1);
+                String nom = result.getString(2);
+                String prenom = result.getString(3);
+
+                String Nom_prenom = nom + " " + prenom;
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
